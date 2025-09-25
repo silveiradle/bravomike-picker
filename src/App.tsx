f5f2681e21b859723de26df7d2e181fd5e780585
Leandro Silveira <leandro-sousa@bm.rs.gov.br>
2025-09-25 11:34:24 -0300

    Final adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom" 
-import { MatchProvider } from "./pages/MatchStore"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { MatchProvider } from "./pages/matchStore"
 import ResultPage from "./pages/ResultPage"
 import SetupPage from "./pages/SetupPage"
 import VetoPage from "./pages/VetoPage"
+import { useEffect } from "react"
 
 export default function App() {
+  useEffect(() => {
+    document.title = "BRAVO.MIKE - Map picker";
+  }, []);
+
   return (
     <MatchProvider>
       <BrowserRouter>
